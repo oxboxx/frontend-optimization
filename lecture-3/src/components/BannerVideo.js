b520a5ad705c20a9df44af436b8c1a2cccac371d
김민재 <lake041@korea.ac.kr>
2024-01-17 17:41:17 +0900

    Lec 03: 동영상 최적화 (#11)
    ## 문제점
    
    - 메인 페이지 동영상의 크기가 52mb로 너무 크다. 동영상 자체가 컨텐츠인 것이 아니라 그저 배경이기 때문에 화질을 낮춰도
    무방하다.
    
    ## 최적화 과정
    
    - ```media.io```에서 webm 확장자로 비트레이트를 512kbps로 낮추고 오디오를 제거했다.
    - mp4 포맷의 경우 비트레이트의 최솟값이 ```900```이어서 이것을 선택했는데, 정확히 무슨 의미인지 모르겠다.
    - 이미지와 마찬가지로 ```video``` 태그 내에서 webm가 지원된다면 webm을, 그렇지 않다면 mp4를 사용하도록
    했다.
    - 낮아진 화질을 보완하기 위해 TailwindCSS의 ```bg-texture```와 ```fliter:
    blur(10px)``` 필터를 적용했다.
    
    ## 참고사항
    
    - mp4 비트레이트를 ```900```으로 설정했더니 오히려 mp4의 용량이 webm보다 낮아졌다.
    - ```900```이 정확히 무슨 의미인지는 모르겠지만, webp와 jpeg에서도 webp의 용량이 더 낮은 경우가 있었던 걸
    보면 webp이나 webm이 반드시 더 용량이 적은 건 아닌 것 같다.
    - webp와 마찬가지로 현재 시점에서는 사파리도 webm이 적용되고 있다.

--- lecture-3/src/components/BannerVideo.js
+++ lecture-3/src/components/BannerVideo.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import video from '../assets/banner-video.mp4'
+import React from "react";
+import video from "../assets/_banner-video.mp4";
+import video_webm from "../assets/_banner-video.webm";
 
 function BannerVideo() {
-	return (
-		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
-			<div className="absolute h-screen w-full left-1/2">
-				<video src={video} className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen" autoPlay loop muted/>
-			</div>
-			<div className="w-full h-full flex justify-center items-center">
-				<div className="text-white text-center">
-					<div className="text-6xl leading-none font-semibold">KEEP</div>
-					<div className="text-6xl leading-none font-semibold">CALM</div>
-					<div className="text-3xl leading-loose">AND</div>
-					<div className="text-6xl leading-none font-semibold">RIDE</div>
-					<div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
+      <div className="absolute h-screen w-full left-1/2">
+        <video
+          className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen"
+          autoPlay
+          loop
+          muted
+          style={{ filter: "blur(10px)" }}
+        >
+          <source src={video_webm} type="video/webm" />
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="text-white text-center">
+          <div className="text-6xl leading-none font-semibold">KEEP</div>
+          <div className="text-6xl leading-none font-semibold">CALM</div>
+          <div className="text-3xl leading-loose">AND</div>
+          <div className="text-6xl leading-none font-semibold">RIDE</div>
+          <div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BannerVideo
+export default BannerVideo;
