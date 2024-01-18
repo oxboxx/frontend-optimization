d1db9bff365525d3200b2384e88ee5f6e4cc086b
lake041 <lake041@korea.ac.kr>
2024-01-19 03:17:35 +0900

    Lec 04: 병목 코드 최적화

--- lecture-4/src/components/PhotoItem.js
+++ lecture-4/src/components/PhotoItem.js
@@ -2,19 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import LazyLoad from 'react-lazyload';
-import { showModal } from '../redux/imageModal';
+import { setBgColor, showModal } from '../redux/imageModal';
+import { getAverageColorOfImage } from '../utils/getAverageColorOfImage';
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = e => {
     dispatch(showModal({ src: urls.full, alt }));
+
+    const averageColor = getAverageColorOfImage(e.target);
+    dispatch(setBgColor(averageColor));
   };
 
   return (
     <ImageWrap>
       <LazyLoad offset={1000}>
-        <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+        <Image
+          src={urls.small + '&t=' + new Date().getTime()}
+          alt={alt}
+          onClick={openModal}
+          crossOrigin="*"
+        />
       </LazyLoad>
     </ImageWrap>
   );
